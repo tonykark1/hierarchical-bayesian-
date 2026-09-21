a0f9bfde03f91742474d7b868e9fd31d28ec4c2e
tonykark1 <tonykark2003@hotmail.com>
2026-09-21 11:55:03 +0300

    Add ridge benchmark targets

--- _targets.R
+++ _targets.R
@@ -13,6 +13,10 @@ list(
     c(26L, 52L, 104L, 156L, 260L, 520L)
   ),
   tar_target(
+    ridge_grid,
+    ridge_lambda_grid(n = 21L)
+  ),
+  tar_target(
     simulation_156,
     simulate_factor_panel(
       n_stocks = 100L,
@@ -29,20 +33,37 @@ list(
     )
   ),
   tar_target(
+    ridge_156,
+    fit_ridge_betas(
+      simulation_156$panel,
+      simulation_156$metadata$factor_names,
+      lambda_grid = ridge_grid
+    )
+  ),
+  tar_target(
     ols_156_joined,
     join_estimates_to_truth(ols_156, simulation_156$truth$stock)
+  ),
+  tar_target(
+    ridge_156_joined,
+    join_estimates_to_truth(ridge_156, simulation_156$truth$stock)
   ),
   tar_target(
     ols_156_summary,
     summarise_beta_recovery(ols_156_joined)
   ),
   tar_target(
-    ols_sample_size_curve,
-    run_ols_recovery_experiment(
+    ridge_156_summary,
+    summarise_beta_recovery(ridge_156_joined)
+  ),
+  tar_target(
+    baseline_sample_size_curve,
+    run_baseline_recovery_experiment(
       sample_sizes = sample_sizes,
       n_stocks = 100L,
       n_sectors = 10L,
-      seed = 123L
+      seed = 123L,
+      lambda_grid = ridge_grid
     )
   )
 )
